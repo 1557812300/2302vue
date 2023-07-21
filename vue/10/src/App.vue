4ba7367da8aa0ad18415b081a772439874819ca5
1096642300 <1096642300@qq.com>
2023-07-21 11:45:23 +0800

    11

---
+++ vue/10/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>App.vue - {{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+// 这是单文件组件，包含三个部分 template  script  style中
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>

62c59bb3a1db428bdf46a26fa437605ce7687479
1096642300 <1096642300@qq.com>
2023-07-21 14:29:25 +0800

    1

---
+++ vue/11/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        msg: 'helloApp...'  
+      }
+    }    
+  }  
+</script>  
+
+<template>  
+  <h2>{{ msg }}</h2>
+</template>
+
+<style scoped>
+
+</style>

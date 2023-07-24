022135117b521836c3d1e2002e26b9d9e3187a6c
1096642300 <1096642300@qq.com>
2023-07-24 11:23:12 +0800

    1

--- vue/12/src/main.ts
+++ vue/12/src/main.ts
@@ -3,5 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // import './ts-anli/01-数据类型'
-import './ts-anli/02-类型断言与接口'
+// import './ts-anli/02-类型断言与接口'
+import './ts-anli/03-函数'
 createApp(App).mount('#app')
---
+++ vue/12/src/ts-anli/03-函数.ts
@@ -0,0 +1,36 @@
+
+
+// 函数声明方式
+// function sum (x:number,y?:number):number {
+//   return x+(y||1)
+// }
+// sum(1,100)
+
+
+// 函数表达式方式
+// const num:number = 100
+// 定义fn函数，有两个参数，并且是number类型，该函数返回是number类型
+// const fn:(x:number,y:number)=>number = (x:number,y:number):number=>x+y
+// fn(1,100)
+
+
+// 通过接口定义函数
+// interface Fn{
+//   ():string
+// }
+// const myFn:Fn= ():string=>'helloworld'
+
+
+// 剩余参数 (展开运算符)
+// function push (array:number[],...items:number[]) {
+//   items.forEach((item:number)=>{
+//     array.push(item)
+//   })
+// }
+// const arr:number[] = []
+// push(arr,4,5,6)
+// console.log(arr)
+
+
+
+

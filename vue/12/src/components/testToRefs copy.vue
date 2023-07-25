0fbe25c0af81c99e80035bdbd10c7aa8985b298c
1096642300 <1096642300@qq.com>
2023-07-25 11:52:05 +0800

    1

---
+++ vue/12/src/components/testToRefs copy.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    test...{{ num }}
+  </div>
+</template>
+
+<script lang='ts'>
+import {defineComponent,onUpdated,toRef,toRefs} from 'vue'
+export default defineComponent({
+  props: {
+    num: {
+      type: Number
+    }
+  },
+  setup (props) {
+    const num = toRef(props,'num')
+    // 数据更新的时候，触发onUpdated钩子函数
+    // onUpdated(()=>{
+    //   console.log('数据改变了---》',props.num)
+    // })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue/12/src/components/testToRefs.vue
+++ vue/12/src/components/testToRefs.vue
@@ -1,32 +1,58 @@
 <template>
   <div>
     test...{{ num }}
+    <button @click="change">孩子更改num数据</button>
   </div>
 </template>
 
-<script lang='ts'>
-import {defineComponent,onUpdated,toRef,toRefs} from 'vue'
-export default defineComponent({
-  props: {
-    num: {
-      type: Number
-    }
-  },
-  // 参数props就是一个Proxy代理对象，不能解构，会失去响应式
-  setup (props) {
-    // console.log('获取外部传入的num属性...',props.num)
-
-    // 如果非要进行解构，需要借助toRefs|toRef处理才可以
-    // const {num} = toRefs(props)
-    const num = toRef(props,'num')
-    console.log('num-->',num.value) // ObjectRefImpl{}
-
-    // 数据更新的时候，触发onUpdated钩子函数
-    // onUpdated(()=>{
-    //   console.log('数据改变了---》',props.num)
-    // })
-  }
+<script lang='ts' setup>
+// 如何获取外部传入的num属性呢？
+import type { PropType } from 'vue'
+interface User{
+  name:string
+  age:number
+}
+const props = defineProps({
+  num: { type: Number, required: true },
+  user: Object as PropType<User>
 })
+
+
+// 实现子父通信，通过defineEits获取emit方法，后续调用它
+// 更改的方法
+// const emit = defineEmits(['changeNum'])
+const emit = defineEmits<{
+  changeNum: [numVal: number,numVal2:number]
+}>()
+const change = ()=>{
+  // 需要触发绑定在自身的自定义事件 changeNum
+  // this.$emit('changeNum',100)
+  emit('changeNum',100,200)
+}
+
+
+// 子组件的某些方法，需要借助defineExpose将其向外暴露
+// 这样的话，父组件通过ref标记后，就可以获取暴漏的方法了
+// say方法
+const say = function () {
+  console.log('child-say...')
+}
+defineExpose({
+  say
+})
+
+
+
+
+// interface User{
+//   name:string
+//   age:number
+// }
+// interface Iprops{
+//   num:number
+//   user:User
+// }
+// const props = defineProps<Iprops>()
 </script>
 
 <style lang="scss" scoped>

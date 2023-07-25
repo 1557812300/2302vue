0259aaa3900dd73ae8ab0e7fcc4bcfb9021683d1
1096642300 <1096642300@qq.com>
2023-07-25 10:08:14 +0800

    1

---
+++ vue/12/src/App-testToRefs.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    <h1 @click="num++">num - {{ num }}</h1>
+    <TestToRefs :num="num"></TestToRefs>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import {defineComponent,ref} from 'vue'
+  import TestToRefs from '@/components/testToRefs.vue'
+  export default defineComponent({
+    components: {
+      TestToRefs
+    },
+    setup () {
+      const num = ref(10)
+      return {
+        num
+      }
+    }
+  })
+
+</script>
+
+
